7d9416b9331fa359b719ca5cca33e46d623d303d
Dario Killenberger <s3787455@student.rmit.edu.au>
2022-10-05 11:08:51 +1100

    added classes

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Welcome. Your options are:");
+        System.out.println(
+        "view packages\n"
+        + "make query\n"
+        + "venue inspection\n"
+        + "login with eventID\n"
+        + "manager login"
+        );
+
+        String userInp = scnr.nextLine();
+        System.out.println();
+
+        if (userInp.equals("manager login")){
+            System.out.println("Enter Username");
+            String userName = scnr.nextLine();
+            System.out.println();
+    
+            System.out.println("Enter Password");
+            String password = scnr.nextLine();
+            System.out.println();
+
+            EventManager.logIn(userName, password);
+
+            if (userName.equals("eventManager")  && password.equals("admin123")) {
+                System.out.printf("welcome " + userName + ". Your options are: \n");
+                EventManager.getOptions(); //Note: No event manager object, since it's not an instantiated class
+            }
+        } 
+        else if (userInp.equals("login with eventID")) {
+            System.out.println("Please enter your event id:");
+            String eventID = scnr.nextLine();
+        }
+
+        scnr.close();
+
+    }   
+}
