7488b5c97eb83f1a34013aef81281e2eb07e5446
DarioKillenberger <s3787455@student.rmit.edu.au>
2022-10-07 23:25:19 +1100

    Moved eventmgr options  into eventmanager class

--- EventManager.java
+++ EventManager.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class EventManager extends Employee
 {
@@ -10,7 +11,7 @@ public class EventManager extends Employee
     }
 
     public void viewComplaint(Query queryobj) {
-        ;
+        queryobj.getQuery();
     }
 
     public void changeVenue() {
@@ -21,14 +22,40 @@ public class EventManager extends Employee
         
     }
 
-    public void getOptions() {
-        System.out.printf(
-            "1. viewQuery\n"
-            + "2. viewComplaint\n"
-            + "3. changeVenue\n"
-            + "4. logout\n"
-            + "Select option: "
-            );
+    public void getOptions(Query query,  String userName) {
+        Scanner scnr = new Scanner(System.in);
+        String userSelect = "";
+    
+        System.out.printf("\nWelcome " + userName + ". Your options are: \n");
+        while (!userSelect.equals("4") && !userSelect.equals("0")) { // If input is 4 (main exit) or 0 (exit after operation complete), stop while loop
+            System.out.printf(
+                "1. viewQuery\n"
+                + "2. viewComplaint\n"
+                + "3. changeVenue\n"
+                + "4. exit\n"
+                + "Select option: "
+                );
+                
+                userSelect = scnr.nextLine();
+                System.out.println();
+                
+                if (userSelect.equals("1")) { // View customer query
+                    // This is the actual query made by the potential customer
+                    query.getQuery();
+                }
+                if (userSelect.equals("2")) { // View customer complaint
+                    query.getQuery();
+                }
+                if (userSelect.equals("3")) { // Change venue
+                    ;
+                }
+                else if (userSelect.equals("4")) { // Logs the manager out and exits
+                    this.logOut();
+                    break;
+                }
+                System.out.print("Operation Complete. Select next action (0 = EXIT, 1 = SHOW ALL OPTIONS AGAIN): ");
+                userSelect = scnr.nextLine();
+            }
     }
     
 }
