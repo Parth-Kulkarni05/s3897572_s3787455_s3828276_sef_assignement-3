21a025b4d65eed962fadc62bf1fef3b210182588
Parth-Kulkarni05 <s3897572@student.rmit.edu.au>
2022-10-09 11:16:51 +1100

    Pushing to pull

---
+++ Refund.java
@@ -0,0 +1,10 @@
+public class Refund{
+
+    private boolean isCanellationAfter2Days = false;
+    private float refundAmount = 0.0f;
+    private boolean refundStatus = false;
+    private String 
+
+
+
+}
